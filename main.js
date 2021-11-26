cd615f9cf1997db13bff6fdfa165ffe726225447
Zulkharnain4510 <71696418+Zulkharnain4510@users.noreply.github.com>
2021-11-26 12:16:38 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+img = "";
+noseX = 0;
+noseY = 0;
+marioX = 325;
+marioY = 325;
+
+function preload()
+{
+  img = loadImage("mario05.png");
+}
+
+function setup() {
+  createCanvas(650, 400);
+}
+
+function draw() {
+  background("#D3D3D3");
+  if(noseX < 300)
+  {
+	  marioX = marioX - 1;
+  }
+  if(noseX > 300)
+  {
+	  marioX = marioX + 1;
+  }
+  if(noseY < 150)
+  {
+	  marioY = marioY - 1;
+  }
+  if(noseX > 300)
+  {
+	  marioX = marioX + 1;
+  }
+  image(img,marioX, marioY, 40,70);
+}
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = creatCapture(1240, 336);
+	cavas.parent('canvas');
+
+	instazileInsetup(mario);
+	
+	video = createCapture(VIDEO);
+	video.size(800,400);
+	video.parent('game_console');
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+
+}
+
+function draw() {
+	game()
+}
+function gotPoses(results)
+{
+	if(results.length > 0)
+	{
+		console.log(result);
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+	}
+}
+
+function modelLoaded() {
+	console.log('Model Loaded!');
+}
